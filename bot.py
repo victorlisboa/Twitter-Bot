39bee48687a3df1871f006a80719481c84e8b2e4
Victor Hugo <62231830+victorlisboa@users.noreply.github.com>
2021-02-04 01:33:09 -0300

    Banco de Dados
    Colocamo banco na budega

--- bot.py
+++ bot.py
@@ -1,6 +1,15 @@
 import tweepy
 from time import sleep
 from random import choice
+from mysql.connector import connection
+
+def create_con():
+    cnx = connection.MySQLConnection(user='bb9ec0ae9cfec0', password='68704f35',
+                              host='us-cdbr-east-03.cleardb.com',
+                              database='heroku_4039ed9669d1447',
+                              use_pure=False)
+    return cnx
+
 
 # Autenticantion
 consumer_key = ''
@@ -36,21 +45,44 @@ frases = ['Você disse Itabuna??? A melhor cidade do mundo!? O paraíso brasilei
 def store_last_seen_id(last_seen_id, op):
     '''Escreve o último ID visto no arquivo correspondente.'''
     if op == 'reply':
-        with open('last_seen_id_reply.txt', 'w') as f:  # Último ID respondido
-            f.write(str(last_seen_id))
+        cnx = create_con()
+        cursor = cnx.cursor()
+        cursor.execute("SELECT * FROM IDs")
+        cursor.fetchall()
+        cursor.execute(f"UPDATE IDs SET ID_REPLY = {last_seen_id} WHERE nome = 'IDs'")
+        cursor.close()
+        cnx.close()
+        print(last_seen_id)
     elif op == 'rt':
-        with open('last_seen_id_rt.txt', 'w') as f:  # Último ID retuitado
-            f.write(str(last_seen_id))
+        cnx = create_con()
+        cursor = cnx.cursor()
+        cursor.execute("SELECT * FROM IDs")
+        cursor.fetchall()
+        cursor.execute(f"UPDATE IDs SET ID_RT = {last_seen_id} WHERE nome = 'IDs'")
+        cursor.close()
+        cnx.close()
+        print(last_seen_id)
 
 
 def get_last_seen_id(op):
     '''Recupera o último ID visto'''
+    cnx = create_con()
+    cursor = cnx.cursor()
+
     if op == 'reply':
-        with open('last_seen_id_reply.txt') as f:
-            return int(f.read())
+        cursor.execute("SELECT * FROM IDs")
+        id = cursor.fetchall()
+        print(id+'REPLY')
+        cursor.close()
+        cnx.close()
+        return id[0][0]
     elif op == 'rt':
-        with open('last_seen_id_rt.txt') as f:
-            return int(f.read())
+        cursor.execute("SELECT * FROM IDs")
+        id = cursor.fetchall()
+        print(id+'RT')
+        cursor.close()
+        cnx.close()
+        return id[0][1]
     
 def reply_to_tweets():
     last_seen_id_reply = get_last_seen_id('reply')
@@ -99,6 +131,6 @@ def retweet():
 
 while True:
     retweet()
-    #reply_to_tweets()
+    reply_to_tweets()
     sleep(15)
 
